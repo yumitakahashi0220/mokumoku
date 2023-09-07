77dedf1e97ccf4c172a13859d56a4d4d67ae783b
yumitakahashi0220 <0ah2.x43726294w@gmail.com>
2023-09-07 13:27:58 +0900

    女性の参加者を増やす修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ApplicationRecord
   has_many :notification_timings, through: :user_notification_timings
   has_one_attached :avatar
 
+  enum gender: { other: 0, male: 1, female: 2 }
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
@@ -75,5 +77,9 @@ class User < ApplicationRecord
 
   def allow_liked_event_notification?
     notification_timings.liked_event.present?
+  end
+
+  def gender_i18n
+    I18n.t("enums.user.gender.#{gender}")
   end
 end
